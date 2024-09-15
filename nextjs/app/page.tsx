0970ba1c20eb1c873597aa7498bac642c2871753
Brandon Hancock <brandon@brandonhancock.io>
2024-09-15 18:04:27 -0400

    first commit for project

---
+++ nextjs/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="bg-red-500 text-white">Hello from new project</div>
+      <Button variant="secondary">Click me</Button>
+    </div>
+  );
+}
